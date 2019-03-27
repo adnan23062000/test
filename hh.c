2628988c76162b31ebd47df7f82e2e087bee2f49
adnan23062000 <48983632+adnan23062000@users.noreply.github.com>
2019-03-27 15:11:27 +0600

    Add files via upload

---
+++ hh.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+char reverse(char s);
+
+int main()
+{
+    int i,j;
+    char s[100],r[100];
+    printf("enter a string:");
+    scanf("%s",&s);
+    int len=strlen(s);
+     printf("Reverse String:%s",reverse(s));
+
+}
+
+
+
+char reverse(char s)
+{
+    int i,j;
+    char r[100];
+    //int len=strlen(s);
+
+     for(i=len-1,j=0;i>=0,j<len;i--,j++){
+        r[j]=s[i];
+    }
+    return r;
+}
